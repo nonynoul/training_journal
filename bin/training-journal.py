bfd414195b498e77425ed1e2b70bb3835c6a04a7
Igor Tsiplakov <factinname@gmail.com>
2026-04-06 16:17:41 +0300

    Update training-journal.py
    Добавлено главное тело

--- bin/training-journal.py
+++ bin/training-journal.py
@@ -5,4 +5,27 @@ from .log import WorkoutLog
 from .workout import Workout, short_workout_info
 
 if __name__ == '__main__':
-    main()
+    log = WorkoutLog()
+
+    while True:
+        print('\nУчёт тренировок')
+        print('1 - добавить тренировку')
+        print('2 - показать тренировки')
+        print('3 - показать статистику')
+        print('0 - выход')
+
+        choice = input('Ваш выбор: ').strip()
+
+        if choice == '1':
+            workout = create_workout()
+            log.add_workout(workout)
+            print('Тренировка сохранена в журнале')
+        elif choice == '2':
+            show_all_workouts(log)
+        elif choice == '3':
+            print(log.statistics_text())
+        elif choice == '0':
+            print('Выход из программы')
+            break
+        else:
+            print('Нет такого пункта')
