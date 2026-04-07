7191aa90fdf31271c296bf03b2a12815240b805f
Igor Tsiplakov <factinname@gmail.com>
2026-04-07 18:47:43 +0300

    Update exercise.py

--- training_journal/exercise.py
+++ training_journal/exercise.py
@@ -17,17 +17,17 @@ class Exercise:
         self.duration = duration
 
     def input_number(text):
-    """
-    Безопасный ввод целого числа
-    :param text: Текст запроса
-    :return: Введённое число
-    """
-    while True:
-        value = input(text).strip()
-        try:
-            return int(value)
-        except ValueError:
-            print('Нужно ввести целое число')
+        """
+        Безопасный ввод целого числа
+        :param text: Текст запроса
+        :return: Введённое число
+        """
+        while True:
+            value = input(text).strip()
+            try:
+                return int(value)
+            except ValueError:
+                print('Нужно ввести целое число')
 
     def __str__(self):
         """
